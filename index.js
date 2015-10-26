cd74df97c6fcd0d681b85bd0fcc18bb5a5a90780
Christine Cha <hello@christinecha.com>
2015-10-26 17:16:41 -0400

    Updated cropping/photo edits.

--- index.js
+++ index.js
@@ -33,37 +33,40 @@ $(document).ready(function(){
     }
 
     if (num == 1) {
-      var current = slider1.getCurrentSlide();
-      var slideQty = slider1.getSlideCount();
-      var randomSlide = Math.round(Math.random() * (slideQty - 1));
-      while (randomSlide == current) {
-        randomSlide = Math.round(Math.random() * (slideQty - 1));
-      };
-      console.log (num, randomSlide);
-      slider1.goToSlide(randomSlide);
+      // var current = slider1.getCurrentSlide();
+      // var slideQty = slider1.getSlideCount();
+      // var randomSlide = Math.round(Math.random() * (slideQty - 1));
+      // while (randomSlide == current) {
+      //   randomSlide = Math.round(Math.random() * (slideQty - 1));
+      // };
+      // console.log (num, randomSlide);
+      // slider1.goToSlide(randomSlide);
+      slider1.goToNextSlide();
     } else if (num == 2) {
-      var current = slider2.getCurrentSlide();
-      var slideQty = slider2.getSlideCount();
-      var randomSlide = Math.round(Math.random() * (slideQty - 1));
-      while (randomSlide == current) {
-        randomSlide = Math.round(Math.random() * (slideQty - 1));
-      };
-      console.log (num, randomSlide);
-      if ((randomSlide == 1) || (randomSlide == 2) || (randomSlide == 5)){
-        $('.image-strip-2 .text h1').css('color', 'black');
-      } else {
-        $('.image-strip-2 .text h1').css('color', 'white');
-      }
-      slider2.goToSlide(randomSlide);
+      // var current = slider2.getCurrentSlide();
+      // var slideQty = slider2.getSlideCount();
+      // var randomSlide = Math.round(Math.random() * (slideQty - 1));
+      // while (randomSlide == current) {
+      //   randomSlide = Math.round(Math.random() * (slideQty - 1));
+      // };
+      // console.log (num, randomSlide);
+      // if ((randomSlide == 1) || (randomSlide == 2) || (randomSlide == 5)){
+      //   $('.image-strip-2 .text h1').css('color', 'black');
+      // } else {
+      //   $('.image-strip-2 .text h1').css('color', 'white');
+      // }
+      // slider2.nex(randomSlide);
+      slider2.goToNextSlide();
     } else if (num == 3) {
-      var current = slider3.getCurrentSlide();
-      var slideQty = slider3.getSlideCount();
-      var randomSlide = Math.round(Math.random() * (slideQty - 1));
-      while (randomSlide == current) {
-        randomSlide = Math.round(Math.random() * (slideQty - 1));
-      };
-      console.log (num, randomSlide);
-      slider3.goToSlide(randomSlide);
+      // var current = slider3.getCurrentSlide();
+      // var slideQty = slider3.getSlideCount();
+      // var randomSlide = Math.round(Math.random() * (slideQty - 1));
+      // while (randomSlide == current) {
+      //   randomSlide = Math.round(Math.random() * (slideQty - 1));
+      // };
+      // console.log (num, randomSlide);
+      // slider3.goToSlide(randomSlide);
+      slider3.goToNextSlide();
     }
   }, 3000);
 
